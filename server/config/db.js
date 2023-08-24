cf968e340d5cbee712d5889ebfa576cdb579f28c
mario ruby <mariogabrielrn@gmail.com>
2023-08-24 13:52:10 +0200

    servidor funcionando en local

--- server/config/db.js
+++ server/config/db.js
@@ -9,3 +9,14 @@ try {
 } catch (err) {
   console.log("Database Not Connected");
 }
+
+// const MONGO_URI =
+// process.env.MONGODB_URI || "mongodb://localhost:27017/Ecommerce";
+// mongoose.connect(MONGO_URI)
+// .then((x)=>{
+//   const dbName = x.connections[0].name;
+//   console.log(`Connected to Mongo! Database name: "${dbName}"`);
+// })
+// .catch((err)=>{
+//   console.error("Error connecting to mongo: ", err);
+// })
